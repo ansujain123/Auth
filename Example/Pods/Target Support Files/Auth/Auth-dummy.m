76c8776c5f01749458ae08abee535e39fb8e4c61
ansu jain <ansu.jain@kuliza.com>
2017-03-08 11:22:21 +0530

    adding Pod Project

---
+++ Example/Pods/Target Support Files/Auth/Auth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Auth : NSObject
+@end
+@implementation PodsDummy_Auth
+@end
---
+++ Example/Pods/Target Support Files/Pods-Auth_Tests/Pods-Auth_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Auth_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Auth_Tests
+@end
